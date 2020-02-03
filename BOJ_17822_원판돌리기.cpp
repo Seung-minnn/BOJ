62430de4deded3106d0ea86a6a8fe7197bab9ebb
Seungmin Ha <48918003+Seung-minnn@users.noreply.github.com>
2020-02-03 10:38:57 +0900

    Update BOJ_17822_원판돌리기.cpp

--- BOJ_17822_원판돌리기.cpp
+++ BOJ_17822_원판돌리기.cpp
@@ -1,217 +1,119 @@
-#include<iostream>
 #include<stdio.h>
+#include<iostream>
 #include<vector>
+#include<deque>
 
 using namespace std;
 
 int n, m, t;
-int ans=0;
+int sum=0, cnt=0;
+const int INF_MIN = -987654321;
 
-vector<vector<int>> pan;	//0~m-1: i¿øÆÇÀÇ ¼ıÀÚµé, m: i¿øÆÇÀÇ ¹æÇâÁ¤º¸(0:¿øÁ¡, +: ½Ã°è, -¹İ½Ã°è)
-vector<vector<int>> plist;	//x,d,k: ¹øÈ£°¡ xÀÇ ¹è¼öÀÎ ¿øÆÇÀ» d¹æÇâ(0:½Ã°è,1:¹İ½Ã°è)À¸·Î kÄ­ ÀÌµ¿
+void find_ans(vector<deque<int>>& p) {
+	vector<vector<bool>> test(n);
+	for (int i = 0; i < n; i++) {
+		test[i].resize(m);
+	}
+	bool is_change = false;
 
-int dx[] = { 0,0,1,-1 };
-int dy[] = { 1,-1,0,0 };
-
-vector<vector<int>> testPan;
-//vector<int> x;
-//vector<int> d;
-//vector<int> k;
-
-void check(int x, int y) {
-	int origin_y = y - pan[x][m] + m;
-
-	for (int k = 0; k < 4; k++) {
-		int nx = x + dx[k];
-		int ny = (origin_y + dy[k]) % m;
-
-		if (nx < 0 || nx >= n || ny < 0 || ny >= m) {
-			continue;
+	//1. ì–‘ ì˜† í™•ì¸
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m - 1; j++) {
+			if (p[i][j] == p[i][j + 1] && p[i][j] != INF_MIN && p[i][j + 1] != INF_MIN) {
+				test[i][j] = true;
+				test[i][j + 1] = true;
+				is_change = true;
+			}
 		}
-		if (pan[x][(origin_y) % m] == pan[nx][ny]) {
-			testPan[x][(origin_y) % m] = 1;
-			testPan[nx][ny] = 1;
-			check(nx, ny);
+		if (p[i][0] == p[i][m - 1] && p[i][0] != INF_MIN && p[i][m - 1] != INF_MIN) {
+			test[i][0] = true;
+			test[i][m - 1] = true;
+			is_change = true;
+		}
+	}
+
+	//2. ìœ„ ì•„ë˜ í™•ì¸
+	for (int j = 0; j < m; j++) {
+		for (int i = 0; i < n - 1; i++) {
+			if (p[i][j] == p[i + 1][j] && p[i][j] != INF_MIN && p[i+1][j] != INF_MIN) {
+				test[i][j] = true;
+				test[i + 1][j] = true;
+				is_change = true;
+			}
+		}
+	}
+	
+	//3. ë³€ê²½ì‹œ ì‚­ì œ
+	if (is_change) {
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				if (test[i][j] == true && p[i][j] != INF_MIN) {
+					sum -= p[i][j];
+					cnt--;
+					p[i][j] = INF_MIN;
+
+				}
+			}
+		}
+	}
+	//4. ë³€ê²½ì•ˆë ì‹œ í‰ê·  êµ¬í•˜ê³  í¬ê¸° ë¹„êµ
+	else {
+		double avg = (double)sum / cnt;
+		
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				if (p[i][j] != INF_MIN) {
+					if (p[i][j] > avg) {
+						p[i][j] -= 1;
+						sum--;
+					}
+					else if (p[i][j] < avg) {
+						p[i][j] += 1;
+						sum++;
+					}
+				}
+			}
 		}
 	}
 }
-
-void find_pan() {
-	int round = t;
-	for (int i = 0; i < t; i++) {
-		int x = plist[i][0];
-		int d = plist[i][1];
-		int k = plist[i][2];
-
-		for (int j = 0; j < n; j++) {
-			if ((j+1) % x != 0) {
-				continue;
-			}
-			if (d == 0) {
-				pan[j][m] = (pan[j][m] + k) % m;
-			}
-			else if(d == 1) {
-				pan[j][m] = (pan[j][m] - k + m) % m;
-			}
-		}
-
-		for (int t1 = 0; t1 < n; t1++) {
-			for (int t2 = 0; t2 < m; t2++) {
-				check(t1, t2);
-
-
-				printf("\n------------------------\n");
-				for (int t3 = 0; t3 < n; t3++) {
-					for (int t4 = 0; t4 < m; t4++) {
-						printf("%d ", pan[t3][t4]);
-					}
-					printf("\n");
-				}
-				printf("\n");
-				for (int t3 = 0; t3 < n; t3++) {
-					for (int t4 = 0; t4 < m; t4++) {
-						printf("%d ", testPan[t3][t4]);
-					}
-					printf("\n");
-				}
-
-
-
-			}
-		}
-		
-		bool tf = false;
-		int tmp_sum = 0, tmp_cnt=0, tmp_avg=0;
-
-		for (int t1 = 0; t1 < n; t1++) {
-			for (int t2 = 0; t2 < m; t2++) {
-				if (pan[t1][t2] != 0) {
-					tmp_sum += pan[t1][t2];
-					tmp_cnt += 1;
-				}
-				if (testPan[t1][t2] == 1) {
-					testPan[t1][t2] = 0;
-					pan[t1][t2] = 0;
-					tf = true;
-				}
-			}
-		}
-		if (!tf) {
-			tmp_avg = tmp_sum / tmp_cnt;
-			for (int t1 = 0; t1 < n; t1++) {
-				for (int t2 = 0; t2 < m; t2++) {
-					if (pan[t1][t2] > tmp_avg) pan[t1][t2] -= 1;
-					else if (pan[t1][t2] < tmp_avg) pan[t1][t2] += 1;
-				}
-			}
-		}
-
-
-		printf("\n------------------------\n");
-		for (int t1 = 0; t1 < n; t1++) {
-			for (int t2 = 0; t2 < m; t2++) {
-				printf("%d ", pan[t1][t2]);
-			}
-			printf("\n");
-		}
-		
-	}
-}
-
 int main() {
-/*
-4 4 1
-1 1 2 3
-5 2 4 2
-3 1 3 5
-2 1 3 2
-2 0 1
-->30
-*/
 	scanf("%d %d %d", &n, &m, &t);
-	
-	for (int i = 0; i < n; i++) {
-		int tmp;
-		pan.push_back(vector<int>());
-		testPan.push_back(vector<int>());
 
-		for (int j = 0; j < m; j++) {
-			scanf("%d", &tmp);
-			pan.at(i).push_back(tmp);
-			testPan.at(i).push_back(0);
-		} 
-		
-		pan.at(i).push_back(0);
+	vector<deque<int>> pan(n);
 
-	}
-	
-	for (int i = 0; i < t; i++) {
-		int tmp;
-		plist.push_back(vector<int>());
-		for (int j = 0; j < 3; j++) {
-			scanf("%d", &tmp);
-			plist.at(i).push_back(tmp);
-		}
-	}
-/*
-	for (int i = 0; i < t; i++) {
-		int t1, t2, t3;
-		scanf("%d %d %d", &t1, &t2, &t3);
-		x.push_back(t1);
-		d.push_back(t2);
-		k.push_back(t3);
-	}
-*/
-
-	find_pan();
-
+	int num;
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
-			ans += pan[i][j];
+			scanf("%d ", &num);
+			sum += num;
+			cnt++;
+			pan[i].push_back(num);
 		}
 	}
 
-	printf("%d", ans);
-	system("pause");
+	int x, d, k;
+	while (t--) {
+		scanf("%d %d %d", &x, &d, &k);
+		for (int i = 1; i*x <= n; i++) {
+			int xx = i * x - 1;
+			for (int rot = 0; rot < k; rot++) {
+				if (d == 0) {
+					int tmp = pan[xx].back();
+					pan[xx].pop_back();
+					pan[xx].push_front(tmp);
+				}
+				else {
+					int tmp = pan[xx].front();
+					pan[xx].pop_front();
+					pan[xx].push_back(tmp);
+				}
+			}
+		}
+		find_ans(pan);
+	}
+
+	printf("%d", sum);
+
+	//system("pause");
 	return 0;
 }
-
-
-
-
-/*
-	//int origin_x = (x - pan[x][m] + n) % n;
-	int origin_y = (y - pan[x][m] + m) % m;
-
-	for (int k = 0; k < 4; k++) {
-		int nx = x + dx[k];
-		int ny = (y + dy[k])%m;
-
-		
-		if (pan[x][y] == 0) {
-			continue;
-		}
-		if (nx < 0 || nx >= n || ny < 0 || ny >= m) {
-			continue;
-		}
-		if (k == 0 || k == 1) {
-			ny = (ny - pan[x][m] + m) % m;
-			if(pan[x][origin_y]==pan[nx][ny]){
-				testPan[x][origin_y] = 1;
-				testPan[nx][ny] = 1;
-				check(nx, ny);
-			}
-		}
-		else {
-			if (pan[nx][ny] == pan[x][origin_y]) {
-				if (testPan[nx][ny] == 1) {
-					continue;
-				}
-				testPan[x][origin_y] = 1;
-				testPan[nx][ny] = 1;
-				check(nx, ny);
-			}
-		}
-	}
-
-*/
