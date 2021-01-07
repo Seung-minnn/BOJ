4502aab5ef66ad1032e4ad1a8459b31be60be735
Seungmin Ha <48918003+Seung-minnn@users.noreply.github.com>
2021-01-07 15:44:48 +0900

    Create BOJ_17086_아기상어2.cpp

---
+++ BOJ_17086_아기상어2.cpp
@@ -0,0 +1,64 @@
+#include<iostream>
+#include<cstdio>
+#include<vector>
+#include<queue>
+#include<cstring>
+using namespace std;
+
+int dx[8] = { 0,1,1,1,0,-1,-1,-1 };
+int dy[8] = { -1,-1,0,1,1,1,0,-1 };
+
+int main() {
+	//initiallize
+	int n, m;
+	cin >> n >> m;
+	
+	int dist[51][51];
+	memset(dist, -1, sizeof(dist));
+
+	int map[51][51];
+	queue<pair<int, int>> q;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cin >> map[i][j];
+			if (map[i][j] == 1) {
+				q.push({ i,j });
+				dist[i][j] = 0;
+			}
+		}
+	}
+
+	//bfs
+	while (!q.empty()) {
+		int x = q.front().first;
+		int y = q.front().second;
+		q.pop();
+
+		for (int i = 0; i < 8; i++) {
+			int nx = x + dx[i];
+			int ny = y + dy[i];
+
+			if (nx < 0 || nx >= n || ny < 0 || ny >= m) {
+				continue;
+			}
+
+			if (dist[nx][ny] != -1) {
+				continue;
+			}
+
+			dist[nx][ny] = dist[x][y] + 1;
+			q.push({ nx,ny });
+		}
+	}
+
+	//print answer 
+	int ans = -1;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			if (dist[i][j] > ans) {
+				ans = dist[i][j];
+			}
+		}
+	}
+	cout << ans;
+}
